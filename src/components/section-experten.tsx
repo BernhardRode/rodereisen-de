d351bf3694261f28ea00de68cf284024f7b59ac8
Bernhard Rode <mail@bernhardrode.de>
2021-07-09 16:04:59 +0200

    feat: no blur and bigger text on expert overview

--- src/components/section-experten.tsx
+++ src/components/section-experten.tsx
@@ -19,8 +19,8 @@ const SectionExperten = ({ data }) => {
                         <ExpertImageFront experte={node} />
                       </div>
                       <div className="w-full absolute hue-rotate-180 top-0 left-0">
-                        <div className="absolute z-20 w-full blur text-center text-white pt-4 pb-4 bottom-0">
-                          <h5 className="text-lg text-white flex flex-col">
+                        <div className="absolute z-20 w-full text-center text-white pt-4 pb-4 bottom-0">
+                          <h5 className="text-5xl text-white flex flex-col">
                             <span className="font-light">
                               {node.frontmatter.name}
                             </span>
